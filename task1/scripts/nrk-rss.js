7d9958ddcb92735561e7255a917d37e1bc055e4a
Benny <hoaben13@student.westerdals.no>
2016-10-11 20:00:20 +0200

    added last code for task1, need some polish!!

--- task1/scripts/nrk-rss.js
+++ task1/scripts/nrk-rss.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     var $btn_science;
     var $btn_culture;
     var $btn_sport;
@@ -8,92 +8,122 @@ $(function(){
 
     var XML_feed_object = null;
 
+    var $main_content;
+
     //init
-    var init = function(){
-      
-        var setHTMLObjects = function(){
+    var init = function () {
+
+        var setHTMLObjects = function () {
             $btn_science = $("#btn_science");
             $btn_culture = $("#btn_culture");
             $btn_sport = $("#btn_sport");
             $btn_menu = $("#btn_menu");
             $dropdown_xml = $("#dropdown_xml");
             $testOutput = $("#testOutput");
-        }();//end setHTMLObjects
+            $main_content = $("#main_content");
+        } ();//end setHTMLObjects
 
-        var setEvents = function(){
+        var setEvents = function () {
             $btn_menu.on("click", generateHTMLFromXML);
 
-            $dropdown_xml.on("click","a.getNewsTerm", function(event){
+            $dropdown_xml.on("click", "a.getNewsTerm", function (event) {
                 var RSS_url = $(this).attr("src");
                 makeNRKCall(RSS_url);
             })
 
 
-        }();//end setEvents
-       getFeedXML();
-    }();//end init
+        } ();//end setEvents
+        getFeedXML();
+    } ();//end init
 
-    function generateHTMLFromXML(){
+    function generateHTMLFromXML() {
         $dropdown_xml.html("");
         $(XML_feed_object)
             .find("article")
-            .each(function(){
+            .each(function () {
                 var title = $("title", this).text();
                 var url = $("url", this).text();
 
                 var newAnchor = $("<a>")
                     .attr(
-                        {
-                            class: "getNewsTerm",
-                            id: title,
-                            src: url
-                        }
+                    {
+                        class: "getNewsTerm",
+                        id: title,
+                        src: url
+                    }
                     )
                     .html(title);
                 var $newList = $("<li>")
                     .append(newAnchor);
                 $dropdown_xml.append($newList);
-          
+
             });
     }
 
-    function getFeedXML(){
+    function getFeedXML() {
         $.ajax({
             method: "GET",
             url: "xml/nrk-rss.xml",
             dataType: "xml",
             async: true,
 
-            beforeSend: function(){
+            beforeSend: function () {
                 console.log("LOADING");
             },
-            success: function(result){
+            success: function (result) {
                 console.log("SUCCESS");
                 XML_feed_object = result;
             },
-            error: function(xhr, statusText, errorMsg){
+            error: function (xhr, statusText, errorMsg) {
                 //Error handling
                 console.log(xhr + " " + statusText + " " + errorMsg);
             },
-            complete: function(){
+            complete: function () {
                 console.log("FUNITO");
             }
         }); // END OF AJAX
     }
 
-    function makeNRKCall(RSS_url){
-         $.ajax(
-                    {
-                        url:"../proxy.php?xml_feed_url=" + RSS_url,
-                        method:"GET",
-                        dataType:"xml"
-                    }
-                )
-                .done(function(result){
-                    $("#testOutput").html("Det gikk bra");
-                })
-                .fail(function(a,b,c){
-                    $("#testOutput").html("Det gikk ikke bra");
-                });
+    function makeNRKCall(RSS_url) {
+        $.ajax(
+            {
+                url: "../proxy.php?xml_feed_url=" + RSS_url,
+                method: "GET",
+                dataType: "xml"
+            }
+        )
+            .done(function (result) {
+                showArticle(result);
+            })
+            .fail(function (a, b, c) {
+                $("#testOutput").html("Det gikk ikke bra");
+            });
+    }
+
+    function showArticle(xml) {
+        $main_content.html("");
+
+        $(xml)
+            .find("item")
+            .each(function () {
+                var title = $("title", this).text();
+                var description = $("description", this).text();
+                var thumbnail = $("enclosure", this).attr("url");
+                var date = $("pubDate", this).text();
+
+                var $newArticle = $("<article>")
+                    .addClass("col-md-4")
+                    .append(
+                    $("<h3>").html(title),
+                    $("<p>").html(date),
+                    $("<img>", {
+                        src: thumbnail,
+                        class: "img-responsive"
+                    }),
+                    $("<p>").html(description)
+
+                    );
+                $main_content.append($newArticle);
+            });
     }
 });
