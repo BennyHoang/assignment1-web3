0d2c4e88e67740c000981262961eb94af26a1da4
Benny <hoaben13@student.westerdals.no>
2016-10-14 14:32:56 +0200

    patch v 1.0

--- task2/scripts/pokedex.js
+++ task2/scripts/pokedex.js
@@ -30,23 +30,90 @@ $(function () {
                 displayPokemon(result);
             })
             .fail(function () {
-                alert("ERROR")
+                alert("Could not find pokemon!")
             })
+    }
+
+    function buildArticle(name, height, weight, type, sprites){
+         var $article = $("<article>");
+                var $thumbnail = $("<div>")
+                    .addClass("thumbnail");
+                var $caption = $("<div>")
+                    .addClass("caption");
+                var $image = $("<img>")
+                    .attr({src: sprites, alt: name})
+                    .addClass("img-responsive");
+                var $title = $("<h3>").html("Name: " + name);
+                var $height = $("<p>").html("Height:" + height);
+                var $weight = $("<p>").html("Weight: " + weight);
+                var $type = $("<p>").html("Type: " + type);          
+
+                $article
+                    .append(
+                        $thumbnail
+                            .append(
+                                $image,
+                                $caption
+                                    .append(
+                                        $title,
+                                        $height,
+                                        $weight,
+                                        $type
+                                        )
+                            )
+                    );         
+                $mainContent.append($article);
+                $article.addClass("col-sm-4");
     }
 
     function displayPokemon(result) {
 
-        
-       
-        $(result)
-            .each(function(){
+                var name = result.name;
+                var height = result.height;
+                var weight = result.weight;
+                var type = result.types[0].type.name;
+                
+                var sprites;
+
+                if(result.sprites.front_shiny_female === null){
+                    sprites = "img/MissingNo.png"
+                    
+                }else{
+                    sprites = result.sprites.front_shiny_female;
+                }
+
+                buildArticle(name, height, weight, type, sprites);
+
+               
+        /*
+        $(result).each(function(){
                 var name = this.name;
                 var height = this.height;
                 var weight = this.weight;
                 var type = result.types[0].type.name;
                 var sprites = this.sprites.front_shiny_female;
-                $mainContent.append(name, height, weight,type, sprites);
+
+                var $article = $("<article>");
+                var $thumbnail = $("<div>")
+                    .addClass("thumbnail");
+                var $caption = $("<div>")
+                    .addClass("caption");
+                var $image = $("<img>")
+                    .attr({src: sprites, alt: name})
+                    .addClass("img-responsive");
+                var $title = $("<h3>").html(name);           
+
+                $article
+                    .append(
+                        $thumbnail
+                            .append(
+                                $image,
+                                $caption
+                                    .append($title)
+                            )
+                    );         
+                $mainContent.append($article);
         });
-        
+        */
     }
 })
