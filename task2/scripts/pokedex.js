61ea3c5adff78a71ffac714a4db754a632deeb4d
Benny <hoaben13@student.westerdals.no>
2016-10-14 22:19:56 +0200

    deleted bootstrap folder and added easier enter buttons

--- task2/scripts/pokedex.js
+++ task2/scripts/pokedex.js
@@ -15,109 +15,110 @@ $(function () {
 
         var setEvents = function () {
             $searchPokemonBtn.on("click", function () {
-                var searchTerm = $searchTermTxt.val();
+                //JSON is case-sensetive. Need to convert the value to lowercase 
+                var searchTerm = $searchTermTxt.val().toLowerCase();
 
                 getPokemon(pokeDexUrl, searchTerm);
                 $searchTermTxt.val("");
             })
+            $searchTermTxt.keypress(function (e) {
+                if (e.which == 13) {//Enter key pressed
+                        $searchPokemonBtn.click();
+                    }
+                });
+            } ();
         } ();
-    } ();
 
-    function getPokemon(url, searchTerm) {
-        url += searchTerm;
-        $.getJSON(url)
-            .done(function (result) {
-                displayPokemon(result);
-            })
-            .fail(function () {
-                alert("Could not find pokemon!")
-            })
-    }
+        function getPokemon(url, searchTerm) {
+            url += searchTerm;
+            $.getJSON(url)
+                .done(function (result) {
+                    displayPokemon(result);
+                })
+                .fail(function () {
+                    $("#myModal").modal('show');
+                })
+        }
+        function buildArticle(name, height, weight, type, sprites) {
+            var $article = $("<article>");
+            var $thumbnail = $("<div>")
+                .addClass("thumbnail");
+            var $caption = $("<div>")
+                .addClass("caption");
+            var $image = $("<img>")
+                .attr({ src: sprites, alt: name })
+                .addClass("img-responsive");
+            var $title = $("<h3>").html("Name: " + name);
+            var $height = $("<p>").html("Height:" + height);
+            var $weight = $("<p>").html("Weight: " + weight);
+            var $type = $("<p>").html("Type: " + type);
 
-    function showModal(){
-        var modal = ''
-    }
-
-    function buildArticle(name, height, weight, type, sprites){
-         var $article = $("<article>");
-                var $thumbnail = $("<div>")
-                    .addClass("thumbnail");
-                var $caption = $("<div>")
-                    .addClass("caption");
-                var $image = $("<img>")
-                    .attr({src: sprites, alt: name})
-                    .addClass("img-responsive");
-                var $title = $("<h3>").html("Name: " + name);
-                var $height = $("<p>").html("Height:" + height);
-                var $weight = $("<p>").html("Weight: " + weight);
-                var $type = $("<p>").html("Type: " + type);          
-
-                $article
+            $article
+                .append(
+                $thumbnail
                     .append(
-                        $thumbnail
-                            .append(
-                                $image,
-                                $caption
-                                    .append(
-                                        $title,
-                                        $height,
-                                        $weight,
-                                        $type
-                                        )
-                            )
-                    );         
-                $mainContent.append($article);
-                $article.addClass("col-sm-4");
-    }
+                    $image,
+                    $caption
+                        .append(
+                        $title,
+                        $height,
+                        $weight,
+                        $type
+                        )
+                    )
+                );
+            $mainContent.append($article);
+            $article.addClass("col-sm-4");
+        }
 
-    function displayPokemon(result) {
+        function displayPokemon(result) {
 
-                var name = result.name;
-                var height = result.height;
-                var weight = result.weight;
-                var type = result.types[0].type.name;
-                
-                var sprites;
+            var name = result.name;
+            var height = result.height;
+            var weight = result.weight;
+            var type = result.types[0].type.name;
 
-                if(result.sprites.front_shiny_female === null){
-                    sprites = "img/MissingNo.png"
-                    
-                }else{
-                    sprites = result.sprites.front_shiny_female;
-                }
+            var sprites;
 
-                buildArticle(name, height, weight, type, sprites);
+            if (result.sprites.front_shiny_female === null) {
+                sprites = "img/MissingNo.png"
 
-               
-        /*
-        $(result).each(function(){
-                var name = this.name;
-                var height = this.height;
-                var weight = this.weight;
-                var type = result.types[0].type.name;
-                var sprites = this.sprites.front_shiny_female;
+            } else {
+                sprites = result.sprites.front_shiny_female;
+            }
 
-                var $article = $("<article>");
-                var $thumbnail = $("<div>")
-                    .addClass("thumbnail");
-                var $caption = $("<div>")
-                    .addClass("caption");
-                var $image = $("<img>")
-                    .attr({src: sprites, alt: name})
-                    .addClass("img-responsive");
-                var $title = $("<h3>").html(name);           
+            buildArticle(name, height, weight, type, sprites);
 
-                $article
-                    .append(
-                        $thumbnail
-                            .append(
-                                $image,
-                                $caption
-                                    .append($title)
-                            )
-                    );         
-                $mainContent.append($article);
-        });
-        */
-    }
-})
+
+            /*
+            $(result).each(function(){
+                    var name = this.name;
+                    var height = this.height;
+                    var weight = this.weight;
+                    var type = result.types[0].type.name;
+                    var sprites = this.sprites.front_shiny_female;
+    
+                    var $article = $("<article>");
+                    var $thumbnail = $("<div>")
+                        .addClass("thumbnail");
+                    var $caption = $("<div>")
+                        .addClass("caption");
+                    var $image = $("<img>")
+                        .attr({src: sprites, alt: name})
+                        .addClass("img-responsive");
+                    var $title = $("<h3>").html(name);           
+    
+                    $article
+                        .append(
+                            $thumbnail
+                                .append(
+                                    $image,
+                                    $caption
+                                        .append($title)
+                                )
+                        );         
+                    $mainContent.append($article);
+            });
+            */
+        }
+    })
